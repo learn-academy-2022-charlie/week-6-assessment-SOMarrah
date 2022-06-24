7b70d29786cb6a0fd40d4fdadabbe2ac166ea9d9
SOMarrah <stephenomarrah@gmail.com>
2022-06-24 09:13:07 -0700

    finished ruby commenting and problem 1 of code challenges

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,17 +14,88 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
-const people = [
-  { name: "ford prefect", occupation: "a hitchhiker" },
-  { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "a radio employee" }
-]
-// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+describe("descriptiveArr", () => {
+  it("returns a takes in an array of objects and returns an array with a sentence about each person with their name capitalized.", () => {
+    const people = [
+      { name: "ford prefect", occupation: "a hitchhiker" },
+      { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+      { name: "arthur dent", occupation: "a radio employee" }]
+      // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+    expect(descriptiveArr(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+})
 
+//Got RED
+// FAIL  ./code-challenges.test.js
+//   descriptiveArr
+//     ✕ returns a takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
+
+//   ● descriptiveArr › returns a takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
+
+//     ReferenceError: descriptiveArr is not defined
 
 // b) Create the function that makes the test pass.
+//create a function called descriptiveArr
+//descriptiveArr takes in an array of objects (objArr) as an input
+  //have to access the array and iterate over it.
+  //have to manipulate the value in the name key. The occupation key just needs the value to be returned.
+  //to access data in an array use bracket notation []. to access data in an object use dot notation (.name)
+    //values in the name key have to be  capitalized
+  //store the values from objArr that have been manipulated/returned in a new array.
+  //return that array
+//the expected output is an array with a descriptive sentence about each person in the objArr with their name capitalized.
+//to iterate over the objArr: forLoop, .map, .forEach
+  //.toUpperCase() method converts a string to have uppercase letters. It doesn't change the original string
+
+// const descriptiveArr = (objArr) => {
+//   const newArr = []
+//   objArr.map(value => newArr.push(value.name.toUpperCase() + value.occupation))
+//   return newArr
+// }
+//first output:
+// +   "FORD PREFECTa hitchhiker",
+// +   "ZAPHOD BEEBLEBROXpresident of the galaxy",
+// +   "ARTHUR DENTa radio employee",
+//this is working as intended. The key name and occupation are being access in the array. They name is being manipulated and the occupation is being added to the end. To fix the output as expected I should refactor to only uppercase the first letters. 
+  //Uppercase first letters:
+    //.split() splits a string into an array of substrings. 
+      //so split the name capitalize index 0
+        //index 0 can be accessed with a slice() method
+    //.join() combines an array back into a string.
+
+// const descriptiveArr = (objArr) => {
+//   const newArr = []
+//   objArr.map(value => newArr.push(value.name.split(" ").slice(0,1).toUpperCase().join() + value.occupation))
+//   return newArr
+// } - This gave me type error. The issue is split makes an array, and i was trying to slice the string in the array but instead slicing the array. if I split the name values into an array, I can then map over it again and target the first letter.
 
 
+
+// const descriptiveArr = (objArr) => {
+//   const newArr = []
+//   objArr.map(value => newArr.push(value.name.split(" ").map(value=> value[0].toUpperCase() + value.slice(1)).join(" ") + " is " + value.occupation + "."))
+//   return newArr
+// } This passed but afterwords I am thinking I could make it simpler by using string interpolation
+
+//i found naming conventions in the parameters a little tough to follow so i changed them to be easier to read
+//created the function that takes in objArr
+const descriptiveArr = (objArr) => {
+  //created a variable the stores the new array
+    //iterate over objArr. for each object do
+  newArr = objArr.map(object => {
+    //return each object in a string interpolation with the value(name) being manipulated and the occupation.
+    return `${object.name.split(" ").map(value => value[0].toUpperCase().concat(value.slice(1))).join(" ")} is ${object.occupation}.`
+  })
+  return newArr 
+}
+
+//Got GREEN
+// PASS  ./code-challenges.test.js
+// descriptiveArr
+//   ✓ returns a takes in an array of objects and returns an array with a sentence about each person with their name capitalized. (1 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
@@ -34,6 +105,15 @@ const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
+// describe("hello", () => {
+
+
+//   it("returns a string that says hi", () => {
+
+
+//     expect(hello()).toEqual("hi")
+//   })
+// })
 
 // b) Create the function that makes the test pass.
 
@@ -46,6 +126,14 @@ const cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
 const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
+// describe("hello", () => {
 
+
+//   it("returns a string that says hi", () => {
+
+
+//     expect(hello()).toEqual("hi")
+//   })
+// })
 
 // b) Create the function that makes the test pass.
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)
+# ---1) The BlogPostsController is a child of ApplicationController. It is the controller for the RESTful routes for BlogPosts class
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2) @posts is an instanced variable that holds all of the data in BlogPost 
     @posts = BlogPost.all
   end
 
-  # ---3)
+  # ---3) show is a method that describes the 'show' RESTful route. It has an instanced variable @post that is assigned to a specific primary key defined by params. The .find method appended to the BlogPost class is what allows the method to search through the data to find that key.
   def show
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4) new is a method that describes the 'new' RESTful route. It has an instanced variable @post that is assigned to a new BlogPost class. The .new method appended to the BlogPost class creates a new instance of that class.
   def new
     @post = BlogPost.new
   end
 
   def create
-    # ---5)
+    # ---5)The instance variable @post is assigned to the class BlogPost with .create method appended to it. The .create method makes a new class with specific parameters defined by blog_post_params(found on line 64). If the creation of BlogPost is valid, the user is redirected to blog_post_path. If not the user is redirected to the new_blog_post_path.
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -34,13 +34,13 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    # ---6)
+    # ---6)@post is an instanced variable assigned to BlogPost class. The .find method searches for a specific value in the class. The parameters are defined as the primary key. 
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7) The instanced variable @post has the .update method appended to it. The .update method changes parameters defined as blog_post_params(found on line 64) and saves it back to the database.
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -54,15 +54,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # ---8)
+      # ---8)redirect_to changes the current view of the user to a specific defined path. blog_post_path is the defined path with the parameters @post. This means the path is to a specific blog post, line 53 assignes @post to a primary key of BlogPost class.
       redirect_to blog_post_path(@post)
     end
   end
 
-  # ---9)
+  # ---9) private is a method that only allows the data below it to be called from within the class it is in. This means everything below line 63 can only be accessed by the BlogPostController class (this class).
   private
   def blog_post_params
-    # ---10)
+    # ---10) The data below is creating strong params. Strong params are a way for developers to limit what a user has access to and what the user can manipulate in the database. The .require method is saying the only class to be accessed is BlogPost. The .permit method is saying the only data in that class to be changed is the tite, and the content.
     params.require(:blog_post).permit(:title, :content)
   end
 end
