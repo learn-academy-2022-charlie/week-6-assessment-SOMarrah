cd72586af1b7f16ae932b042ed329180f1cd9577
SOMarrah <stephenomarrah@gmail.com>
2022-06-25 12:05:56 -0700

    finished code challenges

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -96,44 +96,126 @@ const descriptiveArr = (objArr) => {
 
 // Test Suites: 1 passed, 1 total
 // Tests:       1 passed, 1 total
+
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
-// Expected output: [ 2, 0, -1, 0 ]
-const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
-// Expected output: [ 2, 1, -1 ]
-
-// describe("hello", () => {
 
 
-//   it("returns a string that says hi", () => {
+describe("remainderArr", () => {
+  it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3. a string that says hi", () => {
+    const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+    // Expected output: [ 2, 0, -1, 0 ]
+    const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+    // Expected output: [ 2, 1, -1 ]
+    expect(remainderArr(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect(remainderArr(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
 
+//Got RED
+// FAIL  ./code-challenges.test.js
+//   descriptiveArr
+//     ✓ returns a takes in an array of objects and returns an array with a sentence about each person with their name capitalized. (1 ms)
+//   remainderArr
+//     ✕ takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3. a string that says hi
 
-//     expect(hello()).toEqual("hi")
-//   })
-// })
+//   ● remainderArr › takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3. a string that says hi
+
+//     ReferenceError: remainderArr is not defined
 
 // b) Create the function that makes the test pass.
+//Create a function called remainderArr
+//the function takes in a mixed data array (mixArr) as an input
+//the expected output is an array with only the remainders of numbers when divided by 3
+//first I would have to iterate over the array and identify number data type
+  //.filter() iterates and returns a subset of an array
+  //typeof can be used to specify number
+  // modulo can be used to divide by a number, and return any of the remainder
 
+// const remainderArr = (mixArr) => {
+//   mixArr.filter(value => {
+//     typeof value === 'number'
+//     return value % 3
+//   })
+// }
+//This was my first attempt. I always have trouble figuring out how to write the conditional for .filter() method. in my mind I am reading it as if the typeof value being filtered is a number return that value % 3, but I get undefined.
+//I will try to rewrite it as a .map() method
+  //have to push the value if it is a number into a new array
+    //push the value % 3
+
+ const remainderArr = (mixArr) => {
+  const newArr =[]
+  mixArr.map(value => {
+    if(typeof value === 'number'){
+      newArr.push(value%3)
+    }
+  })
+  return newArr
+ }
+
+//Got GREEN
+// PASS  ./code-challenges.test.js
+//   descriptiveArr
+//     ✓ returns a takes in an array of objects and returns an array with a sentence about each person with their name capitalized. (1 ms)
+//   remainderArr
+//     ✓ takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3. a string that says hi
+
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const cubeAndSum1 = [2, 3, 4]
-// Expected output: 99
-const cubeAndSum2 = [0, 5, 10]
-// Expected output: 1125
-// describe("hello", () => {
 
+describe("sumCubeArr", () => {
+  it("takes in an array of numbers and returns the sum of all the numbers cubed.", () => {
+  const cubeAndSum1 = [2, 3, 4]
+  // Expected output: 99
+  const cubeAndSum2 = [0, 5, 10]
+  // Expected output: 1125
+    expect(sumCubeArr(cubeAndSum1)).toEqual(99)
+    expect(sumCubeArr(cubeAndSum2)).toEqual(1125)
+  })
+})
 
-//   it("returns a string that says hi", () => {
+//Got RED
+// FAIL  ./code-challenges.test.js
+//   descriptiveArr
+//     ✓ returns a takes in an array of objects and returns an array with a sentence about each person with their name capitalized. (1 ms)
+//   remainderArr
+//     ✓ takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3. a string that says hi
+//   arrCubed
+//     ✕ takes in an array of numbers and returns the sum of all the numbers cubed.
 
+//   ● arrCubed › takes in an array of numbers and returns the sum of all the numbers cubed.
 
-//     expect(hello()).toEqual("hi")
-//   })
-// })
+//     ReferenceError: arrCubed is not defined
 
 // b) Create the function that makes the test pass.
+//create a function called arrCubed
+//it takes in an array of numbers(numArr) as an input
+  //iterate over the array
+    //forEach()? take each value and cube it
+    //then add all the values in the array together
+    //return the sum
+//expected output is all numbers cubed then added together (2**3 + 3**3 + 4**4)= 99
+
+const sumCubeArr= (numArr) => {
+  newArr = numArr.map(value => value ** 3)
+  return newArr.reduce((a, b) => a + b, 0)//I found reduce method online. it iterates through the array, stores each value and then adds the next value to the previous one, returning only a number.
+} 
+
+//Got GREEN
+// PASS  ./code-challenges.test.js
+//   descriptiveArr
+//     ✓ returns a takes in an array of objects and returns an array with a sentence about each person with their name capitalized. (1 ms)
+//   remainderArr
+//     ✓ takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3. a string that says hi (1 ms)
+//   sumCubeArr
+//     ✓ takes in an array of numbers and returns the sum of all the numbers cubed.
+
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
